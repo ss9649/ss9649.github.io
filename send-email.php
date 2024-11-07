4dbb45c63cb709b9d3257b44bce2a59288853c09
ss9649 <ss9649@g.rit.edu>
2024-11-06 19:53:19 -0500

    trying again

--- send-email.php
+++ send-email.php
@@ -1,21 +1,40 @@
+
 <?php
 
-
-
-if(isset($_POST['submit']))
-{
+if(isset($_POST['submit'])) {
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
     $mailTo = "shobhit2004@yahoo.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an email from ".$name.".\n\n".$message;
+    $mailFrom = $email; // Define mailFrom
+    $headers = "From: " . $mailFrom;
+    $txt = "You have received an email from " . $name . ".\n\n" . $message;
 
-    mail($mailTo, $txt, $headers);
+    mail($mailTo, "New message", $txt, $headers);
 
     header("Location: index.html");
+    exit();
 }
+
+// if(isset($_POST['submit']))
+// {
+//     $name = $_POST["name"];
+//     $email = $_POST["email"];
+//     $message = $_POST["message"];
+
+//     $mailTo = "shobhit2004@yahoo.com";
+//     $headers = "From: ".$mailFrom;
+//     $txt = "You have received an email from ".$name.".\n\n".$message;
+
+//     mail($mailTo, $txt, $headers);
+
+//     header("Location: index.html");
+// }
 
 
 // web server serves web pages, SMTP server serves emails
+?>
+
+
+
